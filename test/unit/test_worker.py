09c003a7882c30b596d33036fc5dd85054c28cf2
Dylan Colli <dfco222@g.uky.edu>
2020-02-10 11:28:53 -0500

    Wrote a test to ensure that monkeypatched attributes did not bleed across tests
    I was worried that monkeypatching might bleed across tests, so I wrote a test to ensure this doesn't happen.

--- test/unit/test_worker.py
+++ test/unit/test_worker.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import copy
+import pytest
 
 
 ROOT = os.path.realpath(os.path.dirname(__file__))
@@ -54,3 +55,11 @@ def test_worker_fig_initialization():
 
   assert (w.figure_method_called), "Figure method was not called!"
   assert (w.plot_method_called), "Make plot method was not called!"
+
+def test_monkeypatch_bleed():
+  """Tests that the monkeypatching done for tests does not bleed across tests"""
+  w = worker.Worker(**WORKER_DICT)
+
+  # This handles the expected attribute error.
+  with pytest.raises(AttributeError):
+    w.figured_method_called
